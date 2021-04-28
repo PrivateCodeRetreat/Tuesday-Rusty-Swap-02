51230997afc510da4a47e4051203b29d5b594e08
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-04-28 18:39:21 +0000

    Do a commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,47 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
 pub fn set_alive() {}
-pub fn next_state(is_alive: bool, number_of_neighbors: i32) -> bool {
-    if number_of_neighbors == 2 && is_alive == false {
+
+pub fn next_state(is_alive: bool, number_of_live_neighbors: i32) -> bool {
+    if number_of_live_neighbors == 2 && is_alive == false {
         false
-    } else if number_of_neighbors == 2 || number_of_neighbors == 3 {
+    } else if number_of_live_neighbors == 2 || number_of_live_neighbors == 3 {
         true
     } else {
         false
+    }
+}
+
+pub fn calculate_live_neighbors(neighbors: [bool; 8]) -> i32 {
+    let mut count = 0;
+
+    for neighbor in neighbors.iter() {
+       if *neighbor{
+           count= count+1;
+       }
+    }
+    return count;
+}
+pub struct Board{
+   stuff: Vec<[i32;2]>
+}
+impl Board{
+    pub fn new() -> Self{
+        return Board{
+            stuff : Vec::new()
+        };
+    }
+
+    pub fn is_alive(&self, x:i32, y:i32) -> bool {
+        return self.stuff.contains(&[x,y]);
+    }
+    pub fn get_neighbors(&self, _x:i32, _y:i32) -> [bool;8]{
+        return [self.is_alive(0,0),
+            self.is_alive(1,0),
+            false,false,false,false,false,false];
+    }
+
+    pub fn set_alive(&mut self, x:i32, y:i32) {
+       self.stuff.push([x,y]);
     }
 }
--- tests/hello-world.rs
+++ tests/hello-world.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
+use hello_world::Board;
+
 #[test]
 fn die_from_underpopulation() {
     assert_eq!(false, hello_world::next_state(true, 1));
@@ -24,4 +26,34 @@ fn dead_cell_with_exactly_three_live_neighbours_live() {
 #[test]
 fn dead_cell_with_two_live_neighbours_stays_dead() {
     assert_eq!(false, hello_world::next_state(false, 2));
+}
+#[test]
+fn calculate_live_neighbours_count() {
+    let neighbors = [true, false, true, false, true, false, false, false];
+    assert_eq!(3, hello_world::calculate_live_neighbors(neighbors));
+}
+#[test]
+fn calculate_live_neighbours_count_4() {
+    let neighbors = [true, true, true, false, true, false, false, false];
+    assert_eq!(4, hello_world::calculate_live_neighbors(neighbors));
+}
+
+#[test]
+fn get_neighbors_from_board() {
+    let board = Board::new();
+    assert_eq!([false,false,false,false,false,false,false,false], board.get_neighbors(1,1))
+}
+
+
+#[test]
+fn get_neighbors_from_board_with_stuff() {
+    let mut board = Board::new();
+    board.set_alive(0,0);
+    assert_eq!([true,false,false,false,false,false,false,false], board.get_neighbors(1,1))
+}
+#[test]
+fn get_neighbors_from_board_with_different_stuff() {
+    let mut board = Board::new();
+    board.set_alive(1,0);
+    assert_eq!([false,true,false,false,false,false,false,false], board.get_neighbors(1,1))
 }
